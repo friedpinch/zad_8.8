1a56eaa6fb59208a8e25f6525b44ded0f120ed22
friedpinch <friedpinch@gmail.com>
2017-04-22 13:30:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+var animal = "Zielone słonie";
+var animalUpperCased = animal.toUpperCase();
+
+var partOfAnimal = text.slice(text.length/2);
+console.log(partOfAnimal);  
+
+
+
+
+
